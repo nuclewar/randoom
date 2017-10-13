aaa02f302746985e1884caef4ec748971a3382ed
nuclewar <32749736+nuclewar@users.noreply.github.com>
2017-10-13 10:40:15 +0300

    Delete footer.php

--- includes/footer.php
+++
@@ -1,8 +0,0 @@
-<!--FOOTER-->
-<div class="row footerBlock darkBack">
-    <div class="col-xs-12 py-5 text-center">
-        <p class="f14 whiteText">
-            &copy; DCS RanDoom 2017
-        </p>
-    </div>
-</div>
